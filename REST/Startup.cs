c0d5a31d4556b35e183ff4b823d2bac6b2289312
Dylan Restivo <dylan.restivo@yahoo.com>
2021-12-20 13:55:58 -0600

    Updated files to previous repo versions.

--- REST/Startup.cs
+++ REST/Startup.cs
@@ -32,15 +32,11 @@ namespace REST
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            services.AddScoped<IApplicantBL, ApplicantBL>();
-            services.AddScoped<IApplicantRepo, ApplicantRepo>();
-            services.AddScoped<IApplicantOccupationRepo, ApplicantOccupationRepo>();
-            services.AddScoped<IApplicantSkillRepo, ApplicantSkillRepo>();
             
             //services.AddScoped<IOccupationRepo, OccupationRepo>();
             services.AddScoped<IClientRepo, ClientRepo>();
             services.AddScoped<INeedRepo, NeedRepo>();
+            services.AddScoped<IOwnerRepo, OwnerRepo>();
             //services.AddScoped<ITopicRepo, TopicRepo>();
             //services.AddScoped<IOrderRepo, OrderRepo>();
             //services.AddScoped<IUserRepo, UserRepo>();
@@ -54,7 +50,10 @@ namespace REST
             //services.AddDbContext<BatchesDBContext>(opt => opt.UseInMemoryDatabase(databaseName: "TestDatabase"));
 
             // TODO use when psql database is good to use
-            services.AddDbContext<BatchesDBContext>(opts => opts.UseNpgsql(Configuration.GetConnectionString("batchesDB")));
+
+              services.AddDbContext<BatchesDBContext>(opts => opts.UseNpgsql(Configuration.GetConnectionString("batchesDB")));
+
+            
 
             services.AddSingleton(_ => Configuration);
 
