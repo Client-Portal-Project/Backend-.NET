06480b212a3e431623ce357f915ffea8060bb321
Michael Mason <mlm@michael-mason.com>
2022-01-03 14:47:12 -0700

    proposed repo changes - generic repository

---
+++ REST/DataLayer/Interfaces/INeedRepo.cs
@@ -0,0 +1,13 @@
+﻿using REST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST.DataLayer.Interfaces
+{
+    public interface INeedRepo : IGenericRepo<Need>
+    {
+        //methods specific to Need table
+    }
+}
